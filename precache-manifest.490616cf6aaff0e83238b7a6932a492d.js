c70c74110d212cb4ce3f1e78f12542aeb5a7551a
Karim MuHamad <karimmuhamadfci@gmail.com>
2019-06-29 09:32:54 +0200

    Updates

---
+++ precache-manifest.490616cf6aaff0e83238b7a6932a492d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d115b271762e924dbb62d118861676a6",
+    "url": "/KiiMuu/React_Template.git/index.html"
+  },
+  {
+    "revision": "27a8b95993c90c41a511",
+    "url": "/KiiMuu/React_Template.git/static/css/main.1c61d313.chunk.css"
+  },
+  {
+    "revision": "2e7dbfa4558173fd58d8",
+    "url": "/KiiMuu/React_Template.git/static/js/2.4a997efd.chunk.js"
+  },
+  {
+    "revision": "27a8b95993c90c41a511",
+    "url": "/KiiMuu/React_Template.git/static/js/main.2f1ecab7.chunk.js"
+  },
+  {
+    "revision": "9fa059bccea70f18d471",
+    "url": "/KiiMuu/React_Template.git/static/js/runtime~main.43a53679.js"
+  },
+  {
+    "revision": "2241b5b2a88a842d4e60dc7fc53a9bec",
+    "url": "/KiiMuu/React_Template.git/static/media/about.2241b5b2.jpg"
+  },
+  {
+    "revision": "f391e657f9cabe91f018ca14e340d693",
+    "url": "/KiiMuu/React_Template.git/static/media/logo.f391e657.svg"
+  },
+  {
+    "revision": "78e3aa55a906e9faaaf0047629be210f",
+    "url": "/KiiMuu/React_Template.git/static/media/slider1.78e3aa55.jpg"
+  },
+  {
+    "revision": "c2dda9ed3f1255d2327d62311ee3c0e1",
+    "url": "/KiiMuu/React_Template.git/static/media/slider2.c2dda9ed.jpeg"
+  },
+  {
+    "revision": "cea8de4e7b00ca8adbc5cd3b5497316f",
+    "url": "/KiiMuu/React_Template.git/static/media/slider3.cea8de4e.jpg"
+  }
+]);
